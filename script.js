795450dba8ef5ac362d5a0ce308d93f115e42114
sprro <j.reynoldsdesign@gmail.com>
2016-12-16 22:11:04 -0800

    Crazy CSS
    Holy crap the CSS is doing things I don’t fully understand yet

---
+++ script.js
@@ -0,0 +1,19 @@
+//Problem: User interaction doesn't provide desired results. 
+//Solution: add interactivity so the user can manage daily taks. 
+
+//Add a new task
+
+
+//Edit an existing task
+
+
+//Mark a task as complete
+
+
+//Mark a task as incomplete
+
+
+//Delete an existing task
+
+
+
